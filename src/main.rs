ea7008d99a552f09831fefa864a1f5a008b7036c
GINTOAHC <chaotingchen10@gmail.com>
2023-05-06 01:11:08 +0800

    refactor: Move random_strength to the module file

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ mod node; // Contains the Node struct and it's implementation
 use node::Node; // Use the Node struct
 
 mod random; // Get the random number
-use random::get_random; // Use the get_random function
+use random::random_strength; // Use the random_strength function
 
 /* 3D Triangular Lattice
  * (h: height, i: 2D i, j: 2D j)
@@ -93,59 +93,6 @@ fn create_vector(jxx: &Jxx) {
             }
         }
     }
-}
-
-fn random_strength(jxx: &Jxx) {
-    #![allow(non_snake_case)]
-    let H: i32 = jxx.h; // Height of the triangular lattice.
-    let L2: i32 = jxx.l * jxx.l; // L^2
-
-    let mut rand_array = vec![0.0; (L2 * 3) as usize]; // Build a array of random numbers
-    for i in 0..(L2 * 3) {
-        rand_array[i as usize] = get_random(100.0);
-    }
-
-    let layer_rand = get_random(100.0);
-
-    unsafe {
-        // Set the nodes' strength to the random values (Only for the first layer)
-        let (mut ndx, mut idx): (usize, usize) = (0, 0);
-        loop {
-            if ndx >= L2 as usize {
-                break;
-            }
-            println!("idx = {}", idx);
-            NODES[ndx].j_right = rand_array[idx];
-            NODES[ndx].j_bottom = rand_array[idx + 1];
-            NODES[ndx].j_btm_right = rand_array[idx + 2];
-            NODES[ndx].j_layer_up = layer_rand;
-            idx += 3;
-            ndx += 1;
-        }
-    }
-
-    unsafe {
-        // Set the nodes' strength to the random values (For the other layers)
-        for h in 1..H {
-            // Set from height == 1
-            let (mut ndx, mut idx): (usize, usize) = (0, 0);
-            loop {
-                if ndx >= L2 as usize {
-                    break;
-                }
-                let nndx: usize = ((h * L2) as usize + ndx) as usize;
-                NODES[nndx].j_right = rand_array[idx];
-                NODES[nndx].j_bottom = rand_array[idx + 1];
-                NODES[nndx].j_btm_right = rand_array[idx + 2];
-                NODES[nndx].j_layer_up = layer_rand;
-                idx += 3;
-                ndx += 1;
-            }
-        }
-    }
-
-    print_node_info();
-    return;
 }
 
 // Main function
--- src/random.rs
+++ src/random.rs
@@ -1,7 +1,63 @@
 use rand::prelude::*;
+use crate::Jxx;
+use crate::NODES;
+use crate::print_node_info;
 
-pub fn get_random(max: f64) -> f64 {
+fn get_random(max: f64) -> f64 {
     let mut rng = thread_rng();
     let result = rng.gen_range(0.0..max);
     return (result * 100.0).round() / 100.0;
+}
+
+pub fn random_strength(jxx: &Jxx) {
+    #![allow(non_snake_case)]
+    let H: i32 = jxx.h; // Height of the triangular lattice.
+    let L2: i32 = jxx.l * jxx.l; // L^2
+
+    let mut rand_array = vec![0.0; (L2 * 3) as usize]; // Build a array of random numbers
+    for i in 0..(L2 * 3) {
+        rand_array[i as usize] = get_random(100.0);
+    }
+
+    let layer_rand = get_random(100.0);
+
+    unsafe {
+        // Set the nodes' strength to the random values (Only for the first layer)
+        let (mut ndx, mut idx): (usize, usize) = (0, 0);
+        loop {
+            if ndx >= L2 as usize {
+                break;
+            }
+            println!("idx = {}", idx);
+            NODES[ndx].j_right = rand_array[idx];
+            NODES[ndx].j_bottom = rand_array[idx + 1];
+            NODES[ndx].j_btm_right = rand_array[idx + 2];
+            NODES[ndx].j_layer_up = layer_rand;
+            idx += 3;
+            ndx += 1;
+        }
+    }
+
+    unsafe {
+        // Set the nodes' strength to the random values (For the other layers)
+        for h in 1..H {
+            // Set from height == 1
+            let (mut ndx, mut idx): (usize, usize) = (0, 0);
+            loop {
+                if ndx >= L2 as usize {
+                    break;
+                }
+                let nndx: usize = ((h * L2) as usize + ndx) as usize;
+                NODES[nndx].j_right = rand_array[idx];
+                NODES[nndx].j_bottom = rand_array[idx + 1];
+                NODES[nndx].j_btm_right = rand_array[idx + 2];
+                NODES[nndx].j_layer_up = layer_rand;
+                idx += 3;
+                ndx += 1;
+            }
+        }
+    }
+
+    print_node_info();
+    return;
 }
